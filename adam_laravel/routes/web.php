ad0dad2ab0f83667c052603a7233b45c88915bfc
Adam-io15 <adamwildana1503@gmail.com>
2026-03-10 22:09:16 +0700

    Acara 3 Routing

--- adam_laravel/routes/web.php
+++ adam_laravel/routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user', function () {
+    return "Halaman User";
+});
+
+Route::get('/user/{id}', function ($id) {
+    return "ID User: " . $id;
+});
+
+Route::get('/user/{id}/{nama}', function ($id, $nama) {
+    return "ID: $id Nama: $nama";
+});
+
+Route::get('/produk/{nama?}', function ($nama = "Laptop") {
+    return "Produk: " . $nama;
+});
+
+Route::redirect('/home','/user');
+
+Route::view('/about','welcome');
